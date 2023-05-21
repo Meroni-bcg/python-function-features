a1dcbe3e0dfb525fa9cb1ac00d492f88827beacb
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-05-21 10:18:25 -0700

    return longest number of strings

--- main.py
+++ main.py
@@ -36,3 +36,12 @@ def arb_mean(*args):
     total += a
   print(a / len(args))
 
+#arb_longest_string - Accepts any number of strings and returns the longest one.
+def arb_longest_string(*args):
+  long = 0
+  longest = ""
+  for a in args:
+    if len(a) > long:
+      long = len(a)
+      longest = a
+  return longest
