6c6bc8448cb25e50d7124fe096efce0befa2bac2
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-05-21 10:10:29 -0700

    take 2 int and perform separate

--- main.py
+++ main.py
@@ -2,3 +2,20 @@
 def arb_args(*args):
   for a in args:
     print(a)
+
+#inner_func - Takes in two integers. Two nested functions should perform separate, distinct math operations using the integers. The result of both nested functions should then be added together and printed.
+
+def inner_func(x, y):
+
+  def inner_1():
+    return x + y
+
+  def inner_2():
+    return x - y
+
+  print(inner_1() + inner_2())
+
+
+  #lunch_lady - Takes in two strings: a student's name and their lunch preference. The function should print both strings. If a lunch preference is not given, "Mystery Meat" should be printed instead.
+
+  
